8ddf51387f53016c3fda06e40999695f6eeb7a5a
jaden-2 <jedidiahamonia1@gmail.com>
2024-12-16 08:30:30 +0100

    Implemented the order entity

--- src/main/java/com/store/restaurants/entity/Order.java
+++ src/main/java/com/store/restaurants/entity/Order.java
@@ -1,4 +1,37 @@
 package com.store.restaurants.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Entity
+@Table(name = "Order", schema ="mydb")
+@Data
+@NoArgsConstructor
 public class Order {
+    public enum  Status {PENDING, COMPLETED};
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Integer orderId;
+
+    @Column(name = "custumer_id")
+    @NonNull
+    private Integer CustumerId;
+
+    @Column(name = "total_price")
+    @NonNull
+    private Double totalPrice;
+
+    @Column(name = "status")
+    @NonNull
+    private Status status;
+
+    @Column(name = "restaurant_id")
+    @NonNull
+    private Integer restaurantId;
+
+
 }
